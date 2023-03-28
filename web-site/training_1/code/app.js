3b0c791e5a49d2857a3b8a1a22533e3f0db3f0ba
DmitryGagarin <112936821+DmitryGagarin@users.noreply.github.com>
2023-03-28 23:32:21 +0300

    doesn't work

---
+++ web-site/training_1/code/app.js
@@ -0,0 +1,31 @@
+const cart = document.querySelector("nav .cart")
+const cartSideBar = document.querySelector(".cart-sidebar")
+const closeCart = document.querySelector(".close-cart")
+const burger = document.querySelector(".burger")
+const menuSideBar = document.querySelector(".menu-sidebar")
+const closeMenu = document.querySelector(".close-menu")
+const cartItemsTotal = document.querySelector(".noi")
+const cartPriceTotal = document.querySelector(".total-amount")
+const cartUi = document.querySelector(".cart-sidebar .cart")
+const totalDiv = document.querySelector(".total-sum")
+const clear = document.querySelector(".clear-cart-btn")
+const cartContent = document.querySelector(".cart-content ")
+
+let Cart = [];
+let ButtonsDOM = [];
+
+cart.addEventListener("Click", function(){
+    cartSideBar.style.transform = "translate(0%)"
+    const bodyOverlay = document.createElement("div")
+    bodyOverlay.classList.add("overlay")
+    setTimeout(function(){
+        document.querySelector("body").append(bodyOverlay)
+    },300)
+})
+
+closeCart.addEventListener("Click", function(){
+    cartSideBar.style.transform = "translate(100%)" 
+    const bodyOverlay = document.querySelector(".overlay")
+    document.querySelector("body").removeChild(bodyOverlay)
+})
+
