a61bd393508138dd7b3e07607ec3fdae4740a734
DmitryGagarin <gagriu@yandex.ru>
2023-04-20 23:54:07 +0300

    django_research

---
+++ django/coolsite/women/views.py
@@ -0,0 +1,23 @@
+from django.http import Http404, HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+
+def index(request): #Http request
+    return HttpResponse("<h1> Main Page </h1>")
+
+def categories(request, catid):   
+    if(request.GET):
+        print(request.GET)
+    else:
+        print("get quary is empty")
+    return HttpResponse(f"<h1> Categories Page </h1> <p> {catid}</p>")
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('home', permanent=True)
+    return HttpResponse(f"<h1> Archives Page </h1> <p> {year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound(f"<h1> Page Not Found </h1>")
+
+
+    
