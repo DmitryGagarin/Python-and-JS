a083df7c7907fc030b90f68243f700d81aba0224
DmitryGagarin <gagriu@yandex.ru>
2023-04-28 23:26:14 +0300

    решил делать в новой папке, какие то конфиликты

--- project/coolsite/coolsite/urls.py
+++ project/coolsite/coolsite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for training_4 project.
+URL configuration for coolsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from trucks.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('trucks/', index)
 ]
---
+++ project/coolsite/trucks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    time_created = models.DateTimeField(auto_now_add=True)
+    
