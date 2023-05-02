17ee382b0e558be24af5aea599f98f64e4bff3ca
DmitryGagarin <gagriu@yandex.ru>
2023-05-02 23:54:53 +0300

    туда сюда сделал и переделал чуть

--- JavaScript/parser/parser.js
+++ JavaScript/parser/parser.js
@@ -1,7 +1,41 @@
-document.querySelector('button').onclick = myClick;
+const main = document.querySelector('.main');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const wrapper = document.createElement('div')
 
-function myClick(){
-    let a = document.querySelector('.input').value
-    console.log(a)
-    document.querySelector('.out').innerHTML += a
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const inputValue = Object.fromEntries(new FormData(e.target))
+    const request = await fetch(`https://api.github.com/users/${inputValue.name}`)
+    if (request.ok){
+        const data = await request.json()
+        wrapper.appendChild(card(data))
+        main.appendChild(wrapper)
+        input.value = " "
+    } else {
+        alert("User doesn't exist")
+    }
+})
+
+function card(profileData){
+    const element = document.createElement('div')
+    element.classList.add('profile')
+    element.innerHTML = `
+    <img class="search-image" src=${profileData.avatar_url}</img>
+    <p class="search-text"><span> Name: </span> ${profileData.name}</p>
+    <p class="search-text"><span> Town: </span> ${profileData.location}</p>
+    <p class="search-text"><span> About: </span> ${profileData.bio}</p>`
+    element.appendChild(deleteButton())
+    return element  
+}
+
+function deleteButton(){
+    const element = document.createElement('button')
+    element.classList.add('delete-button')
+    element.innerText = "Delete"
+    element.addEventListener('click', (e) => {
+        wrapper.innerHTML = ' '
+    }) 
+    return element
 }
