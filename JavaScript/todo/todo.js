3fc45a82ab9cfa1a0567d5541e6a861488c7b023
DmitryGagarin <gagriu@yandex.ru>
2023-05-25 23:20:33 +0300

    + decos but still need functions and decos

--- JavaScript/todo/todo.js
+++ JavaScript/todo/todo.js
@@ -1,8 +1,22 @@
-const addButton = document.querySelector(".add-button")
-
+const addButton = document.querySelector('.add-button');
 
 addButton.addEventListener('click', () => {
-    const newDiv = document.createElement('div');
-    newDiv.textContent = 'This is a new div';
-    container.appendChild(newDiv);
-});
+  const checkbox = document.createElement('input')
+  const checkLabel = document.createElement('label')
+  const textbox = document.createElement('input')
+
+  checkbox.type = 'checkbox'
+  checkbox.classList.add('check-box')
+
+  checkLabel.classList.add('check-label')
+
+  textbox.type = 'text'
+  textbox.classList.add('text-box')
+
+
+  const container = document.querySelector('.container')
+
+  container.appendChild(checkbox)
+  container.appendChild(textbox)
+  container.appendChild(checkLabel)
+})
