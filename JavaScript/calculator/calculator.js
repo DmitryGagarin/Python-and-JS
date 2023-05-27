986ca1cca1a3a75a660b8bc6c39af43a570cc4c8
DmitryGagarin <gagriu@yandex.ru>
2023-05-28 00:12:47 +0300

    короче я ваще не вдупляю как это сделать

--- JavaScript/calculator/calculator.js
+++ JavaScript/calculator/calculator.js
@@ -1,31 +1,17 @@
-const input = document.getElementsByClassName("left-box-input")[0]; // getElementsByClassName return a collection
-const output = document.getElementsByClassName('right-box-output')[0]
-
-var dollar = document.querySelector("input[value='dollar-input']")
-var euro = document.querySelector("input[value='euro-input']")
-var yuan = document.querySelector("input[value='yuan-input']")
-
-
-input.addEventListener('input', () => {
-    const inputValue = input.value // 
-    
-    let currency = 2
-
-    const regex = /^[0-9]*$/; // only allow numbers
-    if (!regex.test(inputValue)) {
-        inputValue.value = value.replace(/[^0-9]/g, ""); // remove non-numeric characters
+function CBR_XML_Daily_Ru(rates) {
+    function trend(current, previous) {
+      if (current > previous) return ' ▲';
+      if (current < previous) return ' ▼';
+      return '';
     }
-
-    if (dollar.checked){
-        currency = 78.99
-    }
-    else if (euro.checked){
-        currency = 84.73
-    }
-    else if (yuan.checked){
-        currency = 11.18
-    }
-
-    output.value = inputValue * currency
-})
-
+      
+    var USDrate = rates.Valute.USD.Value.toFixed(4).replace('.', ',');
+    var USD = document.getElementById('USD');
+    USD.innerHTML = USD.innerHTML.replace('00,0000', USDrate);
+    USD.innerHTML += trend(rates.Valute.USD.Value, rates.Valute.USD.Previous);
+  
+    var EURrate = rates.Valute.EUR.Value.toFixed(4).replace('.', ',');
+    var EUR = document.getElementById('EUR');
+    EUR.innerHTML = EUR.innerHTML.replace('00,0000', EURrate);
+    EUR.innerHTML += trend(rates.Valute.EUR.Value, rates.Valute.EUR.Previous);
+  }
